9ab39d377970eed1269fdedb18cf1221b2302422
Joe Ibershoff <joe@ibershoff.com>
2014-09-12 09:47:10 -0400

    copied a migration file from user-management so map can stand alone, and added optional jdbc connection parameter passing to flyway script

--- flyway/doFlyway.sh
+++ flyway/doFlyway.sh
@@ -31,12 +31,19 @@ echo "DB_PASSWORD: $DB_PASSWORD"
 DB_CONNECTION=$(echo "$PARSE_URL" | cut -f3 -d/ | cut -f2 -d@)
 echo "DB_CONNECTION: $DB_CONNECTION"
 
-DB_NAME=$(echo "$PARSE_URL" | cut -f4 -d/)
+DB_NAME=$(echo "$PARSE_URL" | cut -f4 -d/ | cut -f1 -d?)
 echo "DB_NAME: $DB_NAME"
+
+DB_PARAMS=$(echo "$PARSE_URL" | cut -f4 -d/ | cut -f2 -d?)
+if [ -n "$DB_PARAMS" ]
+then
+  DB_PARAMS=?${DB_PARAMS}
+fi
+echo "DB_PARAMS: $DB_PARAMS"
 
 echo
 echo "Attempting Flyway command:"
-FLYWAY_CMD="./flyway -url=jdbc:$DB_TYPE://$DB_CONNECTION/$DB_NAME -user=$DB_USER -password=$DB_PASSWORD -validateOnMigrate=false -outOfOrder=true -locations=filesystem:./sql/$SQL_LOCATION -initOnMigrate=true $@"
+FLYWAY_CMD="./flyway -url=jdbc:${DB_TYPE}://${DB_CONNECTION}/${DB_NAME}${DB_PARAMS} -user=${DB_USER} -password=${DB_PASSWORD} -validateOnMigrate=false -outOfOrder=true -locations=filesystem:./sql/${SQL_LOCATION} -initOnMigrate=true $@"
 echo "$FLYWAY_CMD"
 
 $FLYWAY_CMD
