f24ebf3d3e2e70582aaf2256bebdf53956072f43
Nick McCready <nmccready@gmail.com>
2015-05-18 12:57:10 -0400

    review changes

--- migrations/userManagement/2015-05-18__cartodb_task_config.sql
+++ migrations/userManagement/2015-05-18__cartodb_task_config.sql
@@ -32,3 +32,29 @@ INSERT INTO jq_task_config (
   '2',
   FALSE
 );
+
+INSERT INTO jq_subtask_config (
+  name,
+  task_name,
+  queue_name,
+  step_num,
+  retry_delay_seconds,
+  retry_max_count,
+  hard_fail_timeouts,
+  hard_fail_after_retries,
+  hard_fail_zombies,
+  warn_timeout_seconds,
+  kill_timeout_seconds
+) VALUES (
+  'wake',
+  'cartodb_wake',
+  'cartodb',
+  '1',
+  '4',
+  '4',
+  FALSE,
+  TRUE,
+  TRUE,
+  60,
+  75
+);
