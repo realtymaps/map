b0466023519bd35e7bb67682262d56d1846ef56c
Joe Ibershoff <joe@ibershoff.com>
2015-06-12 16:00:16 -0400

    MAPD-405 corrected empty transform to NULL

--- migrations/userManagement/2015-06-12__normalization_config.sql
+++ migrations/userManagement/2015-06-12__normalization_config.sql
@@ -1,5 +1,5 @@
 UPDATE data_normalization_config
-  SET transform = '', required = FALSE
+  SET transform = NULL, required = FALSE
   WHERE data_source_id = 'swflmls' AND list = 'hidden' AND output = 'Listing On Internet YN';
 
 SELECT easy_normalization_insert ('swflmls', 'base', 'deleted', FALSE, '"Listing On Internet YN"', $$
