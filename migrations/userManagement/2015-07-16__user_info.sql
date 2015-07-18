3be686e2fb7ed847015e252a17e4543ea96db2ed
Nick McCready <nmccready@gmail.com>
2015-07-18 16:24:14 -0400

    working schema addons

--- migrations/userManagement/2015-07-16__user_info.sql
+++ migrations/userManagement/2015-07-16__user_info.sql
@@ -6,30 +6,32 @@ CREATE TABLE account_use_types (
 )
 WITH (OIDS=FALSE);
 
-insert into account_use_types (type, description) values ('realtor', 'I\'m a realtor.');
-insert into account_use_types (type, description) values ('real estate developer', 'I\'m real estate developer.');
-insert into account_use_types (type, description) values ('real estate investor', 'I\'m real estate investor.');
-insert into account_use_types (type, description) values ('property manager', 'I\'m property manager.');
-insert into account_use_types (type, description) values ('own residence', 'I\'m doing research to buy or sell my own residence.');
-
+insert into account_use_types (type, description)
+values
+('realtor', 'I''m a realtor.'),
+('real estate developer', 'I''m real estate developer.'),
+('real estate investor', 'I''m real estate investor.'),
+('property manager', 'I''m property manager.'),
+('own residence', 'I''m doing research to buy or sell my own residence.'),
+('staff', 'I''m a staff memeber.');
 
 CREATE TABLE account_images (
 	"id" SERIAL NOT NULL,
 	"blob" bytea,
-  "gravatar_url" varchar
+  "gravatar_url" varchar,
 	PRIMARY KEY ("id")
 )
 WITH (OIDS=FALSE);
 
 
-#root images for a user account
+-- root images for a user account
 alter table auth_user add column account_image_id int4;
-ALTER TABLE auth_user_profile ADD CONSTRAINT "fk_auth_user_account_image_id" FOREIGN KEY (account_image_id) references account_images(id) ON UPDATE CASCADE ON DELETE SET NULL,
+ALTER TABLE auth_user ADD CONSTRAINT "fk_auth_user_account_image_id" FOREIGN KEY (account_image_id) references account_images(id) ON UPDATE CASCADE ON DELETE SET NULL;
 
-#profile images for a user account which can override what client sees if it
-#is derrived from a profile account
+-- profile images for a user account which can override what client sees if it
+-- is derrived from a profile account
 alter table auth_user_profile add column account_image_id int4;
-ALTER TABLE auth_user_profile ADD CONSTRAINT "fk_auth_user_profile_account_image_id" FOREIGN KEY (account_image_id) references account_images(id) ON UPDATE CASCADE ON DELETE SET NULL,
+ALTER TABLE auth_user_profile ADD CONSTRAINT "fk_auth_user_profile_account_image_id" FOREIGN KEY (account_image_id) references account_images(id) ON UPDATE CASCADE ON DELETE SET NULL;
 
 CREATE TABLE company (
 	"id" SERIAL NOT NULL,
@@ -46,13 +48,12 @@ CREATE TABLE company (
 WITH (OIDS=FALSE);
 
 ALTER TABLE company ADD CONSTRAINT "fk_company_us_state_id" FOREIGN KEY (us_state_id) REFERENCES auth_user (id) ON UPDATE CASCADE ON DELETE SET NULL;
+
 alter table auth_user add column us_state_id int4;
 ALTER TABLE auth_user ADD CONSTRAINT "fk_auth_user_us_state_id" FOREIGN KEY (us_state_id) REFERENCES auth_user (id) ON UPDATE CASCADE ON DELETE SET NULL;
-
-ALTER TABLE auth_user ADD COLUMN us_state_id int4;
-ALTER TABLE auth_user ADD CONSTRAINT "fk_user_us_state_id" FOREIGN KEY (us_state_id) REFERENCES auth_user (id) ON UPDATE CASCADE ON DELETE SET NULL;
-
 alter table auth_user add column address_1 varchar;
 alter table auth_user add column address_2 varchar;
 alter table auth_user add column zip varchar;
 alter table auth_user add column website_url varchar;
+alter table auth_user add column account_use_type_id int;
+ALTER TABLE auth_user ADD CONSTRAINT "fk_auth_user_account_use_type_id" FOREIGN KEY (account_use_type_id) REFERENCES account_use_types (id) ON UPDATE CASCADE ON DELETE SET NULL;
