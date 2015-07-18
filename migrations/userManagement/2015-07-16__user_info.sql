bbb1485dbbf7f20bf98770367108a95e05095642
Nick McCready <nmccready@gmail.com>
2015-07-18 16:08:46 -0400

    schema updates for users and us state

---
+++ migrations/userManagement/2015-07-16__user_info.sql
@@ -0,0 +1,58 @@
+CREATE TABLE account_use_types (
+	"id" SERIAL NOT NULL,
+	"type" varchar,
+  "description" varchar,
+	PRIMARY KEY ("id")
+)
+WITH (OIDS=FALSE);
+
+insert into account_use_types (type, description) values ('realtor', 'I\'m a realtor.');
+insert into account_use_types (type, description) values ('real estate developer', 'I\'m real estate developer.');
+insert into account_use_types (type, description) values ('real estate investor', 'I\'m real estate investor.');
+insert into account_use_types (type, description) values ('property manager', 'I\'m property manager.');
+insert into account_use_types (type, description) values ('own residence', 'I\'m doing research to buy or sell my own residence.');
+
+
+CREATE TABLE account_images (
+	"id" SERIAL NOT NULL,
+	"blob" bytea,
+  "gravatar_url" varchar
+	PRIMARY KEY ("id")
+)
+WITH (OIDS=FALSE);
+
+
+#root images for a user account
+alter table auth_user add column account_image_id int4;
+ALTER TABLE auth_user_profile ADD CONSTRAINT "fk_auth_user_account_image_id" FOREIGN KEY (account_image_id) references account_images(id) ON UPDATE CASCADE ON DELETE SET NULL,
+
+#profile images for a user account which can override what client sees if it
+#is derrived from a profile account
+alter table auth_user_profile add column account_image_id int4;
+ALTER TABLE auth_user_profile ADD CONSTRAINT "fk_auth_user_profile_account_image_id" FOREIGN KEY (account_image_id) references account_images(id) ON UPDATE CASCADE ON DELETE SET NULL,
+
+CREATE TABLE company (
+	"id" SERIAL NOT NULL,
+	"address_1" varchar,
+  "address_2" varchar,
+  "zip" varchar,
+  "us_state_id" int,
+  "phone" varchar,
+  "fax" varchar,
+  "website_url" varchar,
+  "account_photo_id" int references account_images(id) ON UPDATE CASCADE ON DELETE SET NULL,
+	PRIMARY KEY ("id")
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE company ADD CONSTRAINT "fk_company_us_state_id" FOREIGN KEY (us_state_id) REFERENCES auth_user (id) ON UPDATE CASCADE ON DELETE SET NULL;
+alter table auth_user add column us_state_id int4;
+ALTER TABLE auth_user ADD CONSTRAINT "fk_auth_user_us_state_id" FOREIGN KEY (us_state_id) REFERENCES auth_user (id) ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE auth_user ADD COLUMN us_state_id int4;
+ALTER TABLE auth_user ADD CONSTRAINT "fk_user_us_state_id" FOREIGN KEY (us_state_id) REFERENCES auth_user (id) ON UPDATE CASCADE ON DELETE SET NULL;
+
+alter table auth_user add column address_1 varchar;
+alter table auth_user add column address_2 varchar;
+alter table auth_user add column zip varchar;
+alter table auth_user add column website_url varchar;
