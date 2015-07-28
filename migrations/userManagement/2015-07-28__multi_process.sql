b3ebcb2956d27920dee06a1759fd90f0e2aca565
Joe Ibershoff <joe@ibershoff.com>
2015-07-28 17:22:07 -0400

    MAPD-507 fixed comment

--- migrations/userManagement/2015-07-28__multi_process.sql
+++ migrations/userManagement/2015-07-28__multi_process.sql
@@ -1,3 +1,2 @@
--- there seems to be a bug in pool2/knex which doesn't play well with multi-processing  :-(
--- https://github.com/myndzi/pool2/issues/13
+-- pool2 bug fixed, multiprocess away!
 UPDATE jq_queue_config SET processes_per_dyno = '2';
