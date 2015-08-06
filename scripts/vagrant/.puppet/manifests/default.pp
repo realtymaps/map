e981306f6dc8e8bab240bf13909f940275fecb43
Nick McCready <nmccready@gmail.com>
2015-08-06 18:52:11 -0400

    works only on vagrant provision

--- scripts/vagrant/.puppet/manifests/default.pp
+++ scripts/vagrant/.puppet/manifests/default.pp
@@ -15,8 +15,6 @@ include '::gnupg'
 include nginx
 
 #RVM
-class { '::rvm':}
-rvm::system_user { www-data: ; vagrant: ;}#FOR SOME CRAZY reason I canot set the order(->||~>) of this as it fucks it up
 rvm_system_ruby {
   $ruby_version:
     ensure      => 'present',
@@ -29,14 +27,15 @@ rvm_gem {
     ensure       => latest,
     require      => Rvm_system_ruby[$ruby_version];
 }->
-#NODE
-class { 'nvm_nodejs':
+class { 'nvm_nodejs':#NODE
   user    => 'vagrant',
   version => '0.12.6',
   npm_version => '2.12.1'
 }->
-#RUN REALTYMAPS APP
-class { 'app':
+class { 'rvm':
+  system_users => ['www-data','vagrant'], #FOR SOME CRAZY reason I canot set the order(->||~>) of this as it fucks it up
+}->
+class { 'app':#RUN REALTYMAPS APP
   user    => 'vagrant',
   node_version => $node_version,
   ruby_version =>  $ruby_version
