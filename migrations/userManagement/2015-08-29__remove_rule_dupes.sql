4f9f6474a20c2f9c9108e1fc01cffb6b9960114e
Justin Taylor <justin@realtymaps.com>
2015-08-29 13:29:07 -0400

    slight fix to sql

--- migrations/userManagement/2015-08-29__remove_rule_dupes.sql
+++ migrations/userManagement/2015-08-29__remove_rule_dupes.sql
@@ -1,8 +1,8 @@
 DELETE FROM data_normalization_config
-WHERE concat(data_source_id, '-', list, '-', output) IN (
+WHERE concat(data_source_id, '-', list, '-', output, '-', ordering) IN (
   SELECT ruleKey
   FROM (
-    SELECT concat(data_source_id, '-', list, '-', output) as ruleKey,
-    ROW_NUMBER() OVER (partition BY data_source_id, list, output ORDER BY concat(data_source_id, '-', list, '-', output)) AS rnum
+    SELECT concat(data_source_id, '-', list, '-', output, '-', ordering) as ruleKey,
+    ROW_NUMBER() OVER (partition BY data_source_id, list, output ORDER BY concat(data_source_id, '-', list, '-', output, '-', ordering)) AS rnum
     FROM data_normalization_config) t
   WHERE t.rnum > 1);
