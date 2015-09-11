3b012f7f81292a1f90532ec0a1ab7d3e1e4c459c
Joe Ibershoff <joe@ibershoff.com>
2015-09-11 10:58:31 -0400

    MAPD-526 fix out-of-order migrations

--- migrations/propertyData/2015-09-10__rename_field_groupings.sql
+++ migrations/propertyData/2015-09-10__rename_field_groupings.sql
@@ -1,8 +1,11 @@
 
 -- use more general names, so they apply to county data as well as MLS
 
-ALTER TABLE mls_data RENAME COLUMN client_groups TO shared_groups;
-ALTER TABLE mls_data RENAME COLUMN realtor_groups TO subscriber_groups;
+ALTER TABLE IF EXISTS mls_data RENAME COLUMN client_groups TO shared_groups;
+ALTER TABLE IF EXISTS mls_data RENAME COLUMN realtor_groups TO subscriber_groups;
+
+ALTER TABLE IF EXISTS normal_listing_data RENAME COLUMN client_groups TO shared_groups;
+ALTER TABLE IF EXISTS normal_listing_data RENAME COLUMN realtor_groups TO subscriber_groups;
 
 ALTER TABLE combined_data RENAME COLUMN client_groups TO shared_groups;
 ALTER TABLE combined_data RENAME COLUMN realtor_groups TO subscriber_groups;
