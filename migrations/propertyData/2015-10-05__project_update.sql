6c83e4c16db9aa1f3cd88cf5870d44a5377fa118
Jesse Stewart <jessestewart@gmail.com>
2015-10-06 14:00:00 -0400

    update project auth_user_id column

--- migrations/propertyData/2015-10-05__project_update.sql
+++ migrations/propertyData/2015-10-05__project_update.sql
@@ -7,3 +7,10 @@ ALTER TABLE project ADD COLUMN "auth_user_id" integer;
 
 ALTER TABLE ONLY project
     ADD CONSTRAINT project_auth_user_id_fkey FOREIGN KEY (auth_user_id) REFERENCES auth_user(id);
+
+UPDATE project p SET
+  auth_user_id = u.auth_user_id
+FROM
+  auth_user_profile u
+WHERE
+  p.id = u.project_id;
