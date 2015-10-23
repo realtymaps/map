88a4e87a2e954c93eebdd2fb5487d0362955f010
jesse stewart <jessestewart@gmail.com>
2015-10-23 11:19:20 -0400

    Drop the old column after all since favorites will likely end up in project

--- migrations/propertyData/2015-10-21__properties_selected.sql
+++ migrations/propertyData/2015-10-21__properties_selected.sql
@@ -4,4 +4,4 @@ update user_project set properties_selected = user_profile.properties_selected
 from user_profile
 where user_profile.project_id = user_project.id;
 
-alter table user_profile rename properties_selected to favorites;
+alter table user_profile drop column properties_selected;
