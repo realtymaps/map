fd00c4ea00dd5c1097acd8e8cc1a233afd1734f9
Joe Ibershoff <joe@ibershoff.com>
2016-01-08 16:30:42 -0500

    MAPD-613 blackknight task fixes

--- migrations/2016-01-08__blackknight_task_tweaks.sql
+++ migrations/2016-01-08__blackknight_task_tweaks.sql
@@ -1,1 +1,3 @@
-UPDATE jq_task_config SET warn_timeout_minutes = 240, kill_timeout_minutes = 300 WHERE name = 'blackknight';
+UPDATE jq_task_config
+SET warn_timeout_minutes = 240, kill_timeout_minutes = 300, active = TRUE
+WHERE name = 'blackknight';
