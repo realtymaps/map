3a597bc39eb71a0641f5d1e0c1980ae5a1c07a19
Jesse <jesse@Jesses-MacBook-Pro-2.local>
2016-04-04 16:58:53 -0400

    fix script syntax

--- migrations/main/2016-04-04__data_source_unique.sql
+++ migrations/main/2016-04-04__data_source_unique.sql
@@ -2,4 +2,4 @@ update config_data_normalization set ordering=22 where data_source_type = 'mls' 
 update config_data_normalization set ordering=23 where data_source_type = 'mls' and list="base" and output="photo_count";
 update config_data_normalization set ordering=24 where data_source_type = 'mls' and list="base" and output="photo_last_mod_time";
 
-UPDATE config_data_normalizationALTER TABLE config_data_normalization ADD CONSTRAINT unique_rule UNIQUE (data_source_id, data_type, list, ordering);
+ALTER TABLE config_data_normalization ADD CONSTRAINT unique_rule UNIQUE (data_source_id, data_type, list, ordering);
