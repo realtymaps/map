63d8d81a4edaa29c100986dc851dcb88919d090b
Jesse <jesse@Jesses-MacBook-Pro-2.local>
2016-04-04 15:19:38 -0400

    Fixed delete rule bug Added unique constraint to rules to avoid duplicate ordering values within a list

---
+++ migrations/main/2016-04-04__data_source_unique.sql
@@ -0,0 +1,5 @@
+update config_data_normalization set ordering=22 where data_source_type = 'mls' and list="base" and output="photo_id";
+update config_data_normalization set ordering=23 where data_source_type = 'mls' and list="base" and output="photo_count";
+update config_data_normalization set ordering=24 where data_source_type = 'mls' and list="base" and output="photo_last_mod_time";
+
+UPDATE config_data_normalizationALTER TABLE config_data_normalization ADD CONSTRAINT unique_rule UNIQUE (data_source_id, data_type, list, ordering);
