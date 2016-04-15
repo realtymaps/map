943f4550da4817177f2f87706c8abd1e3d8b2823
Joe Ibershoff <joe@ibershoff.com>
2016-04-15 09:59:45 -0400

    config_data_source_lookups improvements

--- migrations/main/2016-04-14__data_source_standardization.sql
+++ migrations/main/2016-04-14__data_source_standardization.sql
@@ -1,6 +1,9 @@
 -- I'm removing data_source_type from the constraint/upsert and queries, so it will be an informational field only
-DROP INDEX config_data_source_fields_SystemName_data_source_id_data_so_idx;
+DROP INDEX "config_data_source_fields_SystemName_data_source_id_data_so_idx";
 CREATE UNIQUE INDEX ON config_data_source_fields ("SystemName", data_source_id, data_list_type);
+
+-- don't need corelogic data slowing queries down
+DELETE FROM config_data_source_lookups WHERE data_source_id = 'corelogic';
 
 -- adding data_source_type as an informational field to be consistent with config_data_source_fields
 ALTER TABLE config_data_source_lookups ADD COLUMN data_source_type TEXT;
