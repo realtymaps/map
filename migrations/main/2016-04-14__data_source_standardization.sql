8de5377e641e7aab4b5414fd4fe7c054db703638
Joe Ibershoff <joe@ibershoff.com>
2016-04-19 16:38:07 -0400

    more fixes

--- migrations/main/2016-04-14__data_source_standardization.sql
+++ migrations/main/2016-04-14__data_source_standardization.sql
@@ -1,11 +1,12 @@
 -- I'm removing data_source_type from the constraint/upsert and queries, so it will be an informational field only
 DROP INDEX "config_data_source_fields_SystemName_data_source_id_data_so_idx";
 CREATE UNIQUE INDEX ON config_data_source_fields ("SystemName", data_source_id, data_list_type);
+ALTER TABLE config_data_source_fields ALTER COLUMN "MetadataEntryID" TYPE TEXT;
 
 
 TRUNCATE config_data_source_lookups;
 -- adding data_source_type as an informational field to be consistent with config_data_source_fields
 ALTER TABLE config_data_source_lookups ADD COLUMN data_source_type TEXT NOT NULL;
 ALTER TABLE config_data_source_lookups ADD COLUMN data_list_type TEXT NOT NULL;
-ALTER TABLE config_data_source_lookups ADD COLUMN "MetadataEntryID" INTEGER;
+ALTER TABLE config_data_source_lookups ADD COLUMN "MetadataEntryID" TEXT;
 CREATE UNIQUE INDEX ON config_data_source_lookups ("LookupName", "Value", data_source_id, data_list_type);
