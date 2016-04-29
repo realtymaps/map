147e7f5ca79b09bab2291f9c75470bd70f81d418
Joe Ibershoff <joe@ibershoff.com>
2016-04-29 11:55:50 -0400

    do blackknight lookup mappings

---
+++ backend/utils/validation/util.validation.numStories.coffee
@@ -0,0 +1,41 @@
+_ = require 'lodash'
+Promise = require 'bluebird'
+DataValidationError = require '../errors/util.error.dataValidation'
+require '../../../common/extensions/strings'
+logger = require '../../config/logger'
+
+module.exports = (options = {}) ->
+  (param, value) -> Promise.try () ->
+    if !value
+      return null
+    parts = []
+    stories = value.match(/\d+(?:\.\d+)?/)
+    if stories?[0]
+      parts.push(stories?[0])
+      if value.indexOf('/L') != -1
+        parts.push('Split Levels')
+      else
+        parts.push('Stories')
+    else if value.indexOf('S/L') != -1
+      parts.push('Split Level')
+    else if value.indexOf('B/L') != -1
+      parts.push('Bi-Level')
+    else if value.indexOf('S/E') != -1
+      parts.push('Split Entry')
+    else if value.indexOf('S/F') != -1
+      parts.push('Split Foyer')
+
+    attic = value.indexOf('A') != -1
+    basement = value.match(/(?:B[^/])|(?:B$)/)?
+    if attic && basement
+      parts.push('with Attic and Basement')
+    else
+      if attic
+        parts.push('with Attic')
+      else if basement
+        parts.push('with Basement')
+
+    if parts.length == 0
+      return value  # just pass it through
+    else
+      return parts.join(' ')
--- migrations/main/2016-04-27__fix_blackknight_types.sql
+++ migrations/main/2016-04-27__fix_blackknight_types.sql
@@ -1,1 +1,36 @@
-UPDATE config_data_source_fields SET "DataType" = 'Character' WHERE
+UPDATE config_data_source_fields SET "DataType" = 'Character' WHERE "SystemName" = '# of Stories';
+UPDATE config_data_source_fields SET "DataType" = 'Decimal' WHERE "SystemName" = 'Parking - # of Cars';
+UPDATE config_data_source_fields SET "DataType" = 'Decimal' WHERE "SystemName" = 'Lot Size - Acres';
+UPDATE config_data_source_fields SET "DataType" = 'Character' WHERE "SystemName" = 'Other Impr Building Area 1';
+UPDATE config_data_source_fields SET "DataType" = 'Character' WHERE "SystemName" = 'Other Impr Building Area 2';
+UPDATE config_data_source_fields SET "DataType" = 'Character' WHERE "SystemName" = 'Other Impr Building Area 3';
+UPDATE config_data_source_fields SET "DataType" = 'Character' WHERE "SystemName" = 'Other Impr Building Area 4';
+UPDATE config_data_source_fields SET "DataType" = 'Character' WHERE "SystemName" = 'Other Impr Building Area 5';
+
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{DataType}', '"Character"'::JSONB, TRUE)::JSON WHERE output = '# of Stories';
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{DataType}', '"Decimal"'::JSONB, TRUE)::JSON WHERE output = 'Parking - # of Cars';
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{DataType}', '"Decimal"'::JSONB, TRUE)::JSON WHERE output = 'Lot Size - Acres';
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{DataType}', '"Character"'::JSONB, TRUE)::JSON WHERE output = 'Other Impr Building Area 1';
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{DataType}', '"Character"'::JSONB, TRUE)::JSON WHERE output = 'Other Impr Building Area 2';
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{DataType}', '"Character"'::JSONB, TRUE)::JSON WHERE output = 'Other Impr Building Area 3';
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{DataType}', '"Character"'::JSONB, TRUE)::JSON WHERE output = 'Other Impr Building Area 4';
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{DataType}', '"Character"'::JSONB, TRUE)::JSON WHERE output = 'Other Impr Building Area 5';
+
+UPDATE
+  config_data_normalization AS normalization SET config = jsonb_set(config::JSONB, '{doLookup}', 'true'::JSONB, TRUE)::JSON
+WHERE
+  EXISTS (
+  SELECT 1 FROM config_data_source_fields AS fields
+  WHERE
+    "fields"."LookupName" IS NOT NULL AND "fields"."LookupName" != ''
+    AND "normalization"."output" = "fields"."LongName"
+    AND "normalization"."data_source_id" = "fields"."data_source_id"
+    AND "normalization"."data_type" = "fields"."data_list_type"
+);
+
+UPDATE config_data_normalization SET config = jsonb_set(config::JSONB, '{doLookup}', 'false'::JSONB, TRUE)::JSON
+WHERE output = '# of Stories';
+UPDATE config_data_normalization SET
+  config = jsonb_set(config::JSONB, '{advanced}', 'true'::JSONB, TRUE)::JSON,
+  transform = '[validators.numStories({})]'
+WHERE output = '# of Stories';
