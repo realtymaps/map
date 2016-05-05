c6690b66e39753b652214153a9f6cc8f737c65ac
nmccready <nmccready@gmail.com>
2016-05-05 19:02:04 -0400

    Reworking Parcel Index migrations (#1176)
    * renamed parcel migrations, along with IF EXISTS, and IF NOT EXISTS checks to help rerunning migrations
    
    * renamed parcel migrations, along with IF EXISTS, and IF NOT EXISTS checks to help rerunning migrations

--- migrations/normalized/2016-05-05__2_parcel_fix_index.sql
+++ migrations/normalized/2016-05-05__2_parcel_fix_index.sql
@@ -1,4 +1,4 @@
-DROP INDEX parcel_data_source_id_data_source_uuid_idx;
+DROP INDEX IF EXISTS parcel_data_source_id_data_source_uuid_idx;
 
 CREATE UNIQUE INDEX parcel_data_source_id_data_source_uuid_batch_id_idx
 ON parcel USING btree (data_source_id, data_source_uuid, batch_id);
