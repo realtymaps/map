6df5acbe009c2c1d5461e9a4772ca5eecb54c4ef
Joe Ibershoff <joe@ibershoff.com>
2016-05-13 10:39:58 -0400

    misc fixes and cleanup

--- migrations/main/2016-05-13__remove_old_bathroom_fields.sql
+++ migrations/main/2016-05-13__remove_old_bathroom_fields.sql
@@ -1,4 +1,6 @@
 
+TRUNCATE TABLE data_combined;
+
 ALTER TABLE data_combined DROP COLUMN IF EXISTS baths_full;
 ALTER TABLE data_combined DROP COLUMN IF EXISTS baths_half;
 ALTER TABLE data_combined DROP COLUMN IF EXISTS baths;
