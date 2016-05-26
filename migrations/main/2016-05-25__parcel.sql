28cef9d1d4cefd11ce8f38d81883d2d08820fd94
nmccready <nemtcan@gmail.com>
2016-05-25 20:20:02 -0400

    fix migration

--- migrations/main/2016-05-25__parcel.sql
+++ migrations/main/2016-05-25__parcel.sql
@@ -9,10 +9,10 @@ DROP INDEX parcel_geom_polys_json_idx;
 DROP INDEX parcel_geom_point_json_idx;
 
 DROP INDEX parcel_data_source_id_inserted_idx;
-DROP INDEX parcel_data_source_id_inserted_idx;
+DROP INDEX parcel_data_source_id_updated_idx;
+DROP INDEX parcel_data_source_id_deleted_idx;
 
 DROP INDEX parcel_data_source_id_idx;
-DROP INDEX parcel_data_source_id_deleted_idx;
 
 DROP INDEX parcel_data_source_id_data_source_uuid_idx;
 DROP INDEX parcel_data_source_id_data_source_uuid_batch_id_idx;
@@ -21,5 +21,5 @@ DROP INDEX parcel_data_source_id_data_source_id_rm_property_id_active_idx;
 DROP INDEX parcel_batch_id_idx;
 
 
-CREATE INDEX parcel_rm_property_id_data_source_id_active
+CREATE INDEX parcel_rm_property_id_data_source_id_active_idx
 ON parcel USING btree (rm_property_id, data_source_id, active);
