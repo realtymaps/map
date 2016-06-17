e72081e651352e1114236e58d5083cf6e7936703
nmccready <nemtcan@gmail.com>
2016-06-17 13:00:26 -0400

    PR fix migraton for CI

--- migrations/ci_main_data/2016-06-09__config_external_account.sql
+++ migrations/ci_main_data/2016-06-09__config_external_account.sql
@@ -1,7 +1,6 @@
 insert into config_external_accounts (name, other, api_key)
-select 'vero', '{
-"auth_token":"crap1$",
-"secret_api_key":"crap2$",
-"public_api_key":"crap3$"
-}', 'crap4$'
-where not exists (select name from config_external_accounts where name = 'vero');
+values ('vero', '{
+"auth_token":"crap1",
+"secret_api_key":"crap2",
+"public_api_key":"crap3"
+}', 'crap4');
