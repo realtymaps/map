5278ae66564a02694ae122d98784fc92658cfdb0
nmccready <nemtcan@gmail.com>
2016-06-20 10:21:37 -0400

    PR fix

--- migrations/ci_main_data/config_external_account.sql
+++
@@ -1,6 +0,0 @@
-insert into config_external_accounts (name, other, api_key)
-values ('vero', '{
-"auth_token":"crap1",
-"secret_api_key":"crap2",
-"public_api_key":"crap3"
-}', 'crap4');
--- migrations/ci_main_data/config_external_accounts.sql
+++ migrations/ci_main_data/config_external_accounts.sql
@@ -7,3 +7,10 @@ INSERT INTO config_external_accounts
   (name, username, password, api_key, other, url, environment)
 VALUES
   ('twilio', 'rmaps_username', 'rmaps_password', 'rmaps_API_KEY', '{"number":"+1-234-567-8910"}', 'rmaps_', NULL);
+
+insert into config_external_accounts (name, other, api_key)
+values ('vero', '{
+"auth_token":"crap1",
+"secret_api_key":"crap2",
+"public_api_key":"crap3"
+}', 'crap4');
