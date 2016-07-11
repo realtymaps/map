3c9a189aa8cd10c54ba4753914c79bd017cc3ab9
Joe Ibershoff <joe@ibershoff.com>
2016-07-11 12:05:52 -0400

    revert clear

--- migrations/main/2016-07-08__clear_finalized_data.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE TABLE data_combined;
-
-DELETE FROM config_keystore WHERE namespace = 'blackknight process dates';
-DELETE FROM config_keystore WHERE namespace = 'data update timestamps' AND "key" != 'digimaps';
