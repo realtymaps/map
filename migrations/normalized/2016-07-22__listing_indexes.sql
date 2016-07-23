4af2cf1572333ba2cc11d3d5f44ccbb8444d8b78
Joe Ibershoff <joe@ibershoff.com>
2016-07-22 23:21:57 -0400

    added explicit index names

--- migrations/normalized/2016-07-22__listing_indexes.sql
+++ migrations/normalized/2016-07-22__listing_indexes.sql
@@ -1,7 +1,7 @@
 
-DROP INDEX listing_batch_id_idx;
-CREATE INDEX ON listing (batch_id, data_source_id);
+DROP INDEX IF EXISTS listing_batch_id_idx;
+CREATE INDEX listing_batch_id_data_source_id_idx ON listing (batch_id, data_source_id);
 
-DROP INDEX listing_rm_property_id_deleted_hide_listing_close_date_idx;
-CREATE INDEX ON listing (rm_property_id, hide_listing, data_source_id, deleted, close_date DESC NULLS FIRST);
+DROP INDEX IF EXISTS listing_rm_property_id_deleted_hide_listing_close_date_idx;
+CREATE INDEX listing_rm_property_id_deleted_hide_listing_data_source_id_close_date_idx ON listing (rm_property_id, hide_listing, data_source_id, deleted, close_date DESC NULLS FIRST);
 
