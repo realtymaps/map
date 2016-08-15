37b6aa4ebfa6c685226936035a93929061bffef1
Jesse Stewart <jesse@pear.local>
2016-08-15 10:59:05 -0400

    Fix migration

--- migrations/normalized/2016-08-15__clear_county.sql
+++ migrations/normalized/2016-08-15__clear_county.sql
@@ -1,4 +1,4 @@
-# This resets county data so that it can be reloaded with fixed deed info
+-- This resets county data so that it can be reloaded with fixed deed info
 SELECT drop_all_tables_like('deed_%');
 SELECT drop_all_tables_like('mortgage_%');
 SELECT drop_all_tables_like('tax_%');
