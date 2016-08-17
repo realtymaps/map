d7a89a6c5a2b2f31e4979b42684bc9eb4219023b
Jesse Stewart <jesse@pear.local>
2016-08-17 18:32:58 -0400

    Add new jobqueue task 'photos' and subtasks Leave old subtasks in place for now since they can be bypassed

---
+++ migrations/main/2016-08-17__photos_jq.sql
@@ -0,0 +1,46 @@
+insert into jq_task_config (
+	name,
+  description,
+  data,
+  repeat_period_minutes,
+  warn_timeout_minutes,
+  kill_timeout_minutes
+)
+values (
+	'photos',
+  'Load MLS photos',
+  '{}',
+  60,
+  12,
+  14
+);
+
+insert into jq_subtask_config (
+	name,
+	task_name,
+	queue_name,
+	step_num,
+	retry_delay_seconds,
+	hard_fail_timeouts,
+	hard_fail_after_retries,
+	hard_fail_zombies,
+	retry_max_count,
+	warn_timeout_seconds,
+	kill_timeout_seconds,
+	auto_enqueue
+)
+(
+	select=
+		name || '_new', 'photos',
+		queue_name,
+		step_num,
+		retry_delay_seconds,
+		hard_fail_timeouts,
+		hard_fail_after_retries,
+		hard_fail_zombies,
+		retry_max_count,
+		warn_timeout_seconds,
+		kill_timeout_seconds,
+		auto_enqueue
+	from jq_subtask_config where name like '%storePhotos%'
+);
