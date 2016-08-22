ed4533c883ab05e1052f2522c0b05f070d11e641
Justin Taylor <justin@realtymaps.com>
2016-08-22 18:23:40 -0400

    used wrong quoting in migration...

--- migrations/main/2016-08-22__parallel_s3.sql
+++ migrations/main/2016-08-22__parallel_s3.sql
@@ -11,4 +11,4 @@ WHERE task_name = 'blackknight' and step_num > 1;
 INSERT INTO jq_subtask_config (
   name, task_name, queue_name, step_num, data, retry_delay_seconds, retry_max_count, hard_fail_timeouts,
   hard_fail_after_retries, hard_fail_zombies, warn_timeout_seconds, kill_timeout_seconds, auto_enqueue, active)
-VALUES ( 'blackknight_copyFile', 'blackknight', 'misc', '2', "null", '30', '10', FALSE, TRUE, FALSE, '240', '300', FALSE, TRUE);
+VALUES ( 'blackknight_copyFile', 'blackknight', 'misc', '2', 'null', '30', '10', FALSE, TRUE, FALSE, '240', '300', FALSE, TRUE);
