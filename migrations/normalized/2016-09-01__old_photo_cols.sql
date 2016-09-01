d2e4b079bd0bc5dd5296b261f88dcde8fd2a9baa
Joe Ibershoff <joe@ibershoff.com>
2016-09-01 16:31:11 -0400

    migration safety

--- migrations/normalized/2016-09-01__old_photo_cols.sql
+++ migrations/normalized/2016-09-01__old_photo_cols.sql
@@ -1,7 +1,7 @@
 ALTER TABLE listing
-DROP COLUMN photos,
-DROP COLUMN cdn_photo,
-DROP COLUMN photo_count,
-DROP COLUMN photo_last_mod_time,
-DROP COLUMN photo_count,
-DROP COLUMN photo_id;
+DROP COLUMN IF EXISTS photos,
+DROP COLUMN IF EXISTS cdn_photo,
+DROP COLUMN IF EXISTS photo_count,
+DROP COLUMN IF EXISTS photo_last_mod_time,
+DROP COLUMN IF EXISTS photo_count,
+DROP COLUMN IF EXISTS photo_id;
