5a77905a66d9ca6b2d72755d352ed97abdc08b76
Joe Ibershoff <joe@ibershoff.com>
2016-09-15 12:51:09 -0400

    bugfix

--- migrations/main/2016-09-15__parcel_indexes.sql
+++ migrations/main/2016-09-15__parcel_indexes.sql
@@ -1,2 +1,2 @@
-CREATE INDEX parcel_bounds_idx ON data_parcel USING GIST (geometry_raw, active) WHERE active = TRUE;
+CREATE INDEX parcel_bounds_idx ON data_parcel USING GIST (geometry_raw);
 CREATE INDEX parcel_carto_idx ON data_parcel (fips_code, active) WHERE active = TRUE;
