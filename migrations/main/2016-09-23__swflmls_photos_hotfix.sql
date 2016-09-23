10d400fbdfb7c53cbd82f7c807b974815a0d0621
Joe Ibershoff <joe@ibershoff.com>
2016-09-23 12:52:38 -0400

    hotfix

--- migrations/main/2016-09-23__swflmls_photos_hotfix.sql
+++ migrations/main/2016-09-23__swflmls_photos_hotfix.sql
@@ -2,7 +2,7 @@ UPDATE config_keystore
 SET value = '1474416000000'  -- beginning of the day on Sept 21, 2016
 WHERE
   key = 'swflmls_photos'
-  AND namespace 'data update timestamps';
+  AND namespace = 'data update timestamps';
 
 
 UPDATE jq_subtask_config
