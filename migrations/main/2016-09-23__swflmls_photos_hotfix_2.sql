1cb706a88b446024255d366a0e38e3e9a87d1988
Joe Ibershoff <joe@ibershoff.com>
2016-09-23 14:43:14 -0400

    save photo_id field

--- migrations/main/2016-09-23__swflmls_photos_hotfix_2.sql
+++ migrations/main/2016-09-23__swflmls_photos_hotfix_2.sql
@@ -1,1 +1,5 @@
-ALTER TABLE retry_photos ADD COLUMN photo_id TEXT NOT NULL;
+ALTER TABLE retry_photos ADD COLUMN photo_id TEXT;
+
+-- need to set the values here
+
+ALTER TABLE retry_photos ALTER COLUMN photo_id SET NOT NULL;
