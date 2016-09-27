4d049ca3ca2c74394a6eb5573167195027686b20
nmccready <nemtcan@gmail.com>
2016-09-27 10:05:49 -0400

    dead file

--- backend/config/cartodb/parcelIndexes.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX idx_parcels_rm_property_id ON parcels USING btree (rm_property_id);
-CREATE INDEX idx_parcels_fips_code_id ON parcels USING btree (fips_code);
-CREATE INDEX idx_the_geom_fips_code_id ON parcels USING gist (the_geom);
