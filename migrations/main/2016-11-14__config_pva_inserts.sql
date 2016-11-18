ec0beb721191c66da98175c45d055252c0974c4c
Joe Ibershoff <joe@ibershoff.com>
2016-11-18 12:07:53 -0500

    bugfix

--- migrations/main/2016-11-14__config_pva_inserts.sql
+++ migrations/main/2016-11-14__config_pva_inserts.sql
@@ -7,14 +7,5 @@ VALUES
   (12003,'http://www.bakerpa.com/GIS/D_SearchResults.asp','{"post": {"GoToPage":"","SearchMenu":"","windowWidth":"1606","windowHeight":"589","H_Parcel":"1","L_Parcel":"","Z_Parcel":"1","OwnerName":"","HouseNumber":"","StreetName":"","PIN":"{{_APN_}}","Section":"","Township":"","Range":"","Use_Code":"","LEGAL":"","Acre_GT":"","Acre_LT":"","HeatedSF_From":"","HeatedSF_To":"","YearBuilt_From":"","YearBuilt_To":"","SaleDateFrom":"","SaleDateTo":"","SalePriceFrom":"","SalePriceTo":"","Sale_Vimp":"","SaleBook":"","SalePage":"","PageCount":"25","button_Search":"Run+Search+>>"}}',FALSE),
   (12005,'http://qpublic6.qpublic.net/fl_display_dw.php?county=fl_bay&KEY={{_APN_}}',NULL,FALSE),
   (12007,'http://www.bradfordappraiser.com/GIS/D_SearchResults.asp','{"post": {"GoToPage":"","SearchMenu":"","windowWidth":"1606","windowHeight":"589","H_Parcel":"1","L_Parcel":"1","Z_Parcel":"1","OwnerName":"","HouseNumber":"","StreetDIR":"","StreetName":"","StreetType":"ANY","PIN":"{{_APN_}}","Subd":"","Section":"","Township":"","Range":"","LEGAL":"","Use_Code":"","Acre_GT":"","Acre_LT":"","HeatedSF_From":"","HeatedSF_To":"","YearBuilt_From":"","YearBuilt_To":"","SalePriceFrom":"","SalePriceTo":"","SaleDateFrom":"","SaleDateTo":"","Sale_Vimp":"","SaleBook":"","SalePage":"","PageCount":"25","button_Search":"Run+Search+>>"}}',FALSE),
-  (12009,'https://www.bcpao.us/PropertySearch/#/nav/Advanced',NULL,TRUE),
-
-  (12005,'',NULL,FALSE),
-  (12005,'',NULL,FALSE),
-  (12005,'',NULL,FALSE),
-  (12005,'',NULL,FALSE),
-  (12003,'','{"post": {""}}',FALSE),
-  (12003,'','{"post": {""}}',FALSE),
-  (12003,'','{"post": {""}}',FALSE),
-  (12003,'','{"post": {""}}',FALSE),
+  (12009,'https://www.bcpao.us/PropertySearch/#/nav/Advanced',NULL,TRUE)
 ;
