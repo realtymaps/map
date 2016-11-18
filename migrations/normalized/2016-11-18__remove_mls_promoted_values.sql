58573501af6e7709e9d26552cdd4483a2503a782
Joe Ibershoff <joe@ibershoff.com>
2016-11-18 15:25:14 -0500

    bugfix

--- migrations/normalized/2016-11-18__remove_mls_promoted_values.sql
+++ migrations/normalized/2016-11-18__remove_mls_promoted_values.sql
@@ -2,5 +2,5 @@
 SELECT for_all_tables_like('tax_%', 'ALTER TABLE @ DROP COLUMN promoted_values');
 
 ALTER TABLE listing DROP COLUMN owner_name;
-ALTER TABLE listing DROP COLUMN owner_name2;
+ALTER TABLE listing DROP COLUMN owner_name_2;
 ALTER TABLE listing DROP COLUMN appraised_value;
