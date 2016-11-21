657bc6b0f8eaad75edbfda0c75d2c5f198d9452a
Joe Ibershoff <joe@ibershoff.com>
2016-11-21 10:51:03 -0500

    bugfix

--- migrations/main/2016-11-21__fix_bk_field_metadata.sql
+++ migrations/main/2016-11-21__fix_bk_field_metadata.sql
@@ -4,7 +4,7 @@ SET "DataType" = 'Decimal'
 WHERE
   data_source_id = 'blackknight'
   AND data_list_type = 'tax'
-  AND SystemName = 'Tax Amount';
+  AND "SystemName" = 'Tax Amount';
 
 UPDATE config_data_normalization
 SET config = '{"DataType":"Decimal","nullZero":true,"nullEmpty":true,"deliminate":true,"addDollarSign":true}'::JSON
