bb52eb41eba90686c6c4f8da39d05c5dd15d656b
Jesse Stewart <jesse@pear.local>
2016-12-23 18:05:34 -0500

    migrations..

--- migrations/main/2016_12_06__user_history.sql
+++ migrations/main/2016_12_06__user_history.sql
@@ -1,7 +1,7 @@
-CREATE TABLE user_history (
-  auth_user_id INT REFERENCES auth_user (id),
-  category TEXT NOT NULL,  -- broad category for an action, something like `account`, `project`, or `mail` 
-  subcategory TEXT NOT NULL,  -- more specific name for the action that compliments category, like `account`-`deactivate` or `mail`-`submitted`
-  description TEXT, -- contains text for notes or description, can often be user defined like a typed reason for `account`-`deactivate`
-  rm_inserted_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now_utc()
-);
+-- CREATE TABLE user_history (
+--   auth_user_id INT REFERENCES auth_user (id),
+--   category TEXT NOT NULL,  -- broad category for an action, something like `account`, `project`, or `mail`
+--   subcategory TEXT NOT NULL,  -- more specific name for the action that compliments category, like `account`-`deactivate` or `mail`-`submitted`
+--   description TEXT, -- contains text for notes or description, can often be user defined like a typed reason for `account`-`deactivate`
+--   rm_inserted_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now_utc()
+-- );
