c0f383f83d102e1af8d8874dde5273069bae322c
Joe Ibershoff <joe@ibershoff.com>
2017-01-05 15:54:20 -0500

    CI fix

---
+++ migrations/ci_main_data/auth_m2m_user_mls.sql
@@ -0,0 +1,7 @@
+insert into auth_m2m_user_mls (mls_code, auth_user_id, mls_user_id, is_verified)
+values(
+  'SWFLMLS',
+  (select id from auth_user where first_name = 'CIRCLE' and last_name = 'CI'),
+  '123456',
+  TRUE
+);
